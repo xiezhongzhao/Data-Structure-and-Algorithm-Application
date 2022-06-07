9437670f315f7b5a1c289348ab355ee16338993c
xiezhongzhao <2234309583@qq.com>
2022-06-07 17:56:01 +0800

    week3完成3道题

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 //
 // Created by ASUS on 2022/5/18.
 //
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+#define ar array
+
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     cout << "hello" << endl;
     return 0;
 }
-
-
 
 
 
