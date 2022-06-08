67cdae345eb84c64128a36e64f31c49d2b09467f
xiezhongzhao <2234309583@qq.com>
2022-06-08 17:59:35 +0800

    week3完成4道题

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    vector<int> a = {-1,-1};
+    cout << a[0] << ", " << a[1] << endl;
     cout << "hello" << endl;
     return 0;
 }
